256e4d67fe1d35e0bab30157f6c46a1bdde0b4b6
CHESTER-ART <74562973+CHESTER-ART@users.noreply.github.com>
2021-01-31 14:11:33 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# prepearing for working eiyh jenkins
+groupadd jenkins
+useradd -d /home/jenkins -m -r -s /bin/bash -g jenkins jenkins
+mount -t nfs -o rw 192.168.37.105:/mnt/IT-Academy/nfs-data/sa2-15-20/Ryabuhin-Artiom/ /mnt/nfs
+#umount /mnt/nfs
+#apt-get update && apt-get install openjdk-8-jdk git maven
+apt-get update -y
+apt-get install openjdk-8-jdk -y
+apt-get install git
+apt-get install maven
+# working with git repo
 cd..
 git init
 git remote add HESK https://github.com/CHESTER-ART/hesk
